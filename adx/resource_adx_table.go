c01f2db3a87374a72e80ef3fa2e9fae5f16190da
Vladimir Lazarenko <favoretti@gmail.com>
2021-03-18 16:22:35 +0100

    Add support for HCL-style table definitions

--- adx/resource_adx_table.go
+++ adx/resource_adx_table.go
@@ -21,7 +21,6 @@ type TableSchema struct {
 	DocString string
 }
 
-
 func resourceADXTable() *schema.Resource {
 	return &schema.Resource{
 		CreateContext: resourceADXTableCreate,
@@ -45,12 +44,37 @@ func resourceADXTable() *schema.Resource {
 
 			"table_schema": {
 				Type:             schema.TypeString,
-				Required:         true,
+				Optional: true,
+				Computed: true,
+				AtLeastOneOf: []string{"table_schema", "column"},
+				ConflictsWith: []string{"column"},
 				ForceNew:         true,
 				ValidateDiagFunc: stringMatch(
 					regexp.MustCompile("[a-zA-Z0-9:-_,]+"),
 					"Table schema must contain only letters, number, dashes, semicolons, commas and underscores and no spaces",
 					),
+			},
+
+			"column": {
+				Type: schema.TypeList,
+				AtLeastOneOf: []string{"table_schema", "column"},
+				ConflictsWith: []string{"table_schema"},
+				ForceNew: true,
+				Optional: true,
+				Elem: &schema.Resource{
+					Schema: map[string]*schema.Schema{
+						"name": {
+							Type:     schema.TypeString,
+							Required: true,
+							ValidateDiagFunc: stringIsNotEmpty,
+						},
+						"type": {
+							Type:     schema.TypeString,
+							Required: true,
+							ValidateDiagFunc: stringIsNotEmpty,
+						},
+					},
+				},
 			},
 		},
 	}
@@ -62,13 +86,17 @@ func resourceADXTableCreate(ctx context.Context, d *schema.ResourceData, meta in
 
 	tableName := d.Get("name").(string)
 	databaseName := d.Get("database_name").(string)
-	tableSchema := d.Get("table_schema").(string)
 
-	// strip any spaces from schema, since ADX returns it back without
-	tableSchema = strings.ReplaceAll(tableSchema, " ", "")
+	tableDef := ""
+
+	if tableSchema := d.Get("table_schema").(string); len(tableSchema) != 0 {
+		tableDef = tableSchema
+	} else {
+		tableDef = expandTableColumn(d.Get("column").([]interface{}))
+	}
 
 	kStmtOpts := kusto.UnsafeStmt(unsafe.Stmt{Add: true})
-	createStatement := fmt.Sprintf(".create table %s (%s)", tableName, tableSchema)
+	createStatement := fmt.Sprintf(".create table %s (%s)", tableName, tableDef)
 
 	_, err := client.Mgmt(ctx, databaseName, kusto.NewStmt("", kStmtOpts).UnsafeAdd(createStatement))
 	if err != nil {
@@ -121,6 +149,7 @@ func resourceADXTableRead(ctx context.Context, d *schema.ResourceData, meta inte
 	d.Set("name", schemas[0].TableName)
 	d.Set("database_name", schemas[0].DatabaseName)
 	d.Set("table_schema", schemas[0].Schema)
+	d.Set("column", flattenTableColumn(schemas[0].Schema))
 
 	return diags
 }
@@ -146,4 +175,34 @@ func resourceADXTableDelete(ctx context.Context, d *schema.ResourceData, meta in
 	d.SetId("")
 
 	return diags
+}
+
+func expandTableColumn(input []interface{}) string {
+	if len(input) == 0 {
+		return ""
+	}
+
+	columns := make([]string, 0)
+	for _, v := range input {
+		block := v.(map[string]interface{})
+		column := fmt.Sprintf("%s:%s", block["name"].(string), block["type"].(string))
+		columns = append(columns, column)
+	}
+	return strings.Join(columns, ",")
+}
+
+func flattenTableColumn(input string) []interface{} {
+	if len(input) == 0 {
+		return []interface{}{}
+	}
+
+	columns := make([]interface{}, 0)
+	for _, v := range strings.Split(input, ",") {
+		block := make(map[string]interface{})
+		fields := strings.Split(v, ":")
+		block["name"] = fields[0]
+		block["type"] = fields[1]
+		columns = append(columns, block)
+	}
+	return columns
 }
--- docs/resources/adx_table.md
+++ docs/resources/adx_table.md
@@ -11,6 +11,8 @@ Manages a table in ADX.
 
 ## Example Usage
 
+You can pass table schema as a string:
+
 ```terraform
 resource "adx_table" "test" {
   name          = "Test1"
@@ -19,11 +21,41 @@ resource "adx_table" "test" {
 }
 ```
 
+Or use HCL to construct it:
+
+```
+resource "adx_table" "test" {
+  name          = "Test1"
+  database_name = "test-db"
+
+  column {
+    name = "f1"
+    type = "string"
+  }
+
+  column {
+    name = "f2"
+    type = "string"
+  }
+
+  column {
+    name = "f3"
+    type = "int"
+  }
+}
+```
+
 ### Argument Reference
 
 - **name** (String, Required) Name of the Table to create. Changing this forces a new resource to be created.
 - **database_name** (String, Required) Database name in which this Table should be created. Changing this forces a new resource to be created.
-- **table_schema** (String, Required) Table schema. Must contain only letters, numbers, dashes, semicolons, commas and underscores and no spaces. Changing this forces a new resource to be created.
+- **table_schema** (String, Optional) Table schema. Must contain only letters, numbers, dashes, semicolons, commas and underscores and no spaces. Changing this forces a new resource to be created.
+- **column** (String, Optional) One or more `column` blocks defined below.
+
+`column` Configures a column and supports the following:
+
+- **name** (String, Required) Column name
+- **type** (String, Required) Column type
 
 ### Attribute Reference
 
